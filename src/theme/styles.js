ccb27c167edf30235095f4aaa5991bde1f11191d
Bao Huynh <bao.huynh@christinas.vn>
2019-12-05 15:44:41 +0700

    [Resource] Add shadow and card to AppStyles

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -9,6 +9,22 @@ export default {
     alignItems: 'center',
     margin: AppSizes.paddingExtraLarge
   },
+  shadow: {
+    shadowColor: AppColors.shadow,
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    elevation: 1,
+    borderRadius: 5
+  },
+  card: {
+    borderColor: AppColors.border,
+    borderRadius: 10,
+    borderWidth: 1
+  },
   icon: {
     width: 24,
     height: 24
