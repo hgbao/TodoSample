00b89800087948516d99a00197de765334662693
Bao Huynh <bao.huynh@christinas.vn>
2019-12-05 14:59:14 +0700

    [Resource] Configure localize and add language resource for en

---
+++ src/config/LanguageConfig.js
@@ -0,0 +1,31 @@
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18n-js';
+import memoize from 'lodash.memoize';
+import { I18nManager } from 'react-native';
+
+const translationGetters = {
+  en: () => require('@translation/en.json')
+};
+
+const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key)
+);
+
+const setI18nConfig = () => {
+  const fallback = { languageTag: 'en', isRTL: false };
+
+  const { languageTag, isRTL } = RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) || fallback;
+
+  translate.cache.clear();
+  I18nManager.forceRTL(isRTL);
+  i18n.translations = { [languageTag]: translationGetters[languageTag]() };
+  i18n.locale = languageTag;
+};
+
+setI18nConfig();
+
+export default {
+  translate,
+  setI18nConfig
+};
---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+import LanguageConfig from './LanguageConfig';
+
+// eslint-disable-next-line import/prefer-default-export
+export { LanguageConfig };
